2bc646b7ca7391d324f1a846532bf49c22326d57
hcxgit <812591452@qq.com>
2019-08-04 20:33:24 +0800

    test1

--- differentSort.py
+++ differentSort.py
@@ -17,6 +17,46 @@ class sorts:
         return
     def heap_sort(self,alist):
         return
+
+    # ----------全排列-------------------
+    def permutations(self, arr, start, end):
+
+        if start == end:
+            # print(arr)
+            pass
+        else:
+            for index in range(start, end):
+                arr[index], arr[start] = arr[start], arr[index]
+                print(index,start)
+                self.permutations(arr, start + 1, end)
+                arr[index], arr[start] = arr[start], arr[index]
+
+
 if __name__ == '__main__':
-    s = sorts
-    s.quick_sort()
+    # s = sorts()
+    # # s.quick_sort()
+    # arr = list(range(1,4))
+    # s.permutations(arr, 0, len(arr))
+
+    import sys
+    res = []
+    while True:
+        n_num = sys.stdin.readline().strip()
+        if not n_num:
+            break
+        n_num = list(map(int,n_num.split()))
+        n_list = list(map(int,sys.stdin.readline().strip().split()))
+
+        allnum = sum(n_list)
+        if allnum%n_num[1] == 0:
+            need = allnum//n_num[1]
+        else:
+            need = (allnum//n_num[1])+1
+        if n_num[2]*60 > need and need<=480:
+            res.append(need)
+        elif allnum-n_num[2] <=480:
+            res.append(allnum-n_num[2])
+        else:
+            res.append(0)
+    for i in res:
+        print(i)
---
+++ 各种总结/pdf/test1.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+
+if __name__ == '__main__':
+    # s = sorts()
+    # # s.quick_sort()
+    # arr = list(range(1,4))
+    # s.permutations(arr, 0, len(arr))
+
+    import sys
+    res = []
+    while True:
+        n_num = sys.stdin.readline().strip()
+        if not n_num:
+            break
+        n_num = list(map(int,n_num.split()))
+        n_list = list(map(int,sys.stdin.readline().strip().split()))
+
+        allnum = sum(n_list)
+        if allnum%n_num[1] == 0:
+            need = allnum//n_num[1]
+        else:
+            need = (allnum//n_num[1])+1
+        if n_num[2]*60 > need and need<=480:
+            res.append(need)
+        elif allnum-n_num[2] <=480:
+            res.append(allnum-n_num[2])
+        else:
+            res.append(0)
+    for i in res:
+        print(i)
