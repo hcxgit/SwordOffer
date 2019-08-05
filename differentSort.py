73c94c3dfbe7dcb22e0051d0acb5b2491b7a220b
hcxgit <812591452@qq.com>
2019-08-05 14:47:00 +0800

    默认

--- differentSort.py
+++ differentSort.py
@@ -56,7 +56,30 @@ class sorts:
 
     # --------------堆排序---------------
     def heap_sort(self,alist):
-        return
+        self.heapSize = len(alist)
+        self.buildHeap(alist)
+
+        # 从将根节点开始，取出第一位数与最后一位做对调，对前面的节点继续进行调整
+        for i in range(self.heapSize-1,-1,-1): # 最后一个节点在往前提
+            alist[i], alist[0] = alist[0], alist[i]
+            self.adjustHeap(alist,0,i)
+
+    def buildHeap(self,alist):   # 建堆
+
+        for i in range((self.heapSize-2)//2,-1,-1):
+            self.adjustHeap(alist,i,self.heapSize)
+
+    def adjustHeap(self,alist,root,last): #在堆中做结构调整使得父节点的值大于子节点
+        left = 2*root+1
+        right = left+1
+        larger = root
+        if left < last and alist[left] > alist[larger]:
+            larger = left
+        if right < last and alist[right] > alist[larger]:
+            larger = right
+        if larger != root:
+            alist[larger],alist[root] = alist[root],alist[larger]
+            self.adjustHeap(alist,larger,last)  # 递归调整动过的子树
 
     # ----------全排列-------------------
     def permutations(self, arr, start, end):
@@ -75,7 +98,7 @@ class sorts:
 if __name__ == '__main__':
     s = sorts()
     a = [4,6,2,1,7,89,13,5,4,6]
-    s.merge_sort(a)
+    s.heap_sort(a)
     print(a)
     # arr = list(range(1,4))
     # s.permutations(arr, 0, len(arr))
