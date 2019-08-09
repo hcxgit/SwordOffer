8a332c29ef73a696d9fb294c0511b2d9a0812665
hcxgit <812591452@qq.com>
2019-08-09 22:20:50 +0800

    leetcode

--- Leetcode/5.最长回文子串.py
+++ Leetcode/5.最长回文子串.py
@@ -7,11 +7,22 @@
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        dp = [1 for i in range(len(s))]  # 以第i个字符为结尾的最长回文子串的长度
-
-        for i in range(len(s)):
-            print(i)
-            return dp
+        length= len(s)
+        if length < 2:
+            return s
+        dp = [[0 for i in range(length)] for j in range(length)]  # dp[i][j]：表示s[i,j]能否构成回文子串(闭环，包括j)
+        
+        res = s[0]
+        max_len = 1
+        for r in range(1,length):
+            for l in range(r):   # 左边 < 右边
+                if s[l] == s[r] and (r-l <=2 or dp[l+1][r-1]): # 左右各缩进1后小于等于1 或者 True
+                    dp[l][r] = True
+                    cur_len = r-l+1
+                    if cur_len > max_len:
+                        max_len = cur_len
+                        res = s[l:r+1]    
+        return res
 
     def isPalindrome(self, s):
         length = len(s)
