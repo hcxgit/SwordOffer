603ab089a60c73e128f3e8108258ac01d4374f8e
hcxgit <812591452@qq.com>
2019-08-09 10:11:48 +0800

    趋势科技题目

--- test1.py
+++ test1.py
@@ -1,30 +1,35 @@
-# coding=utf-8
+import numpy as np
 
-if __name__ == '__main__':
 
-    # 大疆
-    import sys
-    res = []
+def gradient_descent(X, y, lr=0.01, threshold=1e-3):
+    params = np.array([0., 0., 0.])
+    th = 1
     while True:
-        n_num = sys.stdin.readline().strip()
-        if not n_num:
+        params2 = params.copy()
+        for x_,y_ in zip(X,y):
+            y2 = params[0] * x_[0] + params[1] * x_[1] + params[2]
+            loss = y2-y_
+            params[0] -= lr * loss*x_[0]
+            params[1] -= lr * loss*x_[1]
+            params[2] -= lr * loss
+        th = params-params2
+        if abs(th[0] < threshold) and abs(th[1] < threshold) and abs(th[2] < threshold):
             break
-        n_num = list(map(int,n_num.split()))
-        n_list = list(map(int,sys.stdin.readline().strip().split()))
+    return params
 
-        allnum = sum(n_list)
-        if allnum%n_num[1] == 0:
-            need = allnum//n_num[1]
-        else:
-            need = (allnum//n_num[1])+1
-        if n_num[2]*60 > need and need<=480:
-            res.append(need)
-        elif allnum-n_num[2] <=480:
-            res.append(allnum-n_num[2])
-        else:
-            res.append(0)
-    for i in res:
-        print(i)
-
-
-
+#
+# Please don't modify any code below.
+#
+if __name__ == "__main__":
+    # Code to generate the data/test case
+    n_samples = 10000
+    x_1, x_2 = np.random.random(n_samples), np.random.random(n_samples)
+    theta_1, theta_2, b = map(float, input().split())
+    # print (theta_1, theta_2, b)
+    X, y = np.vstack([x_1, x_2]).T, theta_1 * x_1 + theta_2 * x_2 + b
+    # Solution
+    params = gradient_descent(X, y)
+    result = []
+    for param in params:
+        result.append('{:.02f}'.format(param))
+    print(' '.join(result))
