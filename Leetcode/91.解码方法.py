70b65b628a4b0e8a24e4b21b69d560164f7b1b86
hcxgit <812591452@qq.com>
2019-08-10 17:49:41 +0800

    leetcode

--- Leetcode/91.解码方法.py
+++ Leetcode/91.解码方法.py
@@ -7,15 +7,19 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         # dp[i]: 以 i为结尾的编码方式的总数
         # dp[i] = dp[i+1] + (1 if ....)  因为0只能和前面的数组和，所以从后往前遍历
-        if s < '1':
-            return 0
-        elif s=='' or '1' <= s < '10':
+        if s=='':
+            return 1 
+        if s[0] == '0':
+            return 0   
+        if 1 <= int(s) < 10 or s=='20':
             return 1
-        elif '10'< s <= '26':
+        if 10< int(s) <= 26:
             return 2
         s1,s2 = 0,0
-        if '0'<s[:1]<='26':
+        if 0<int(s[:1])<=26:
             s1 = self.numDecodings(s[1:])
-        if '0'<s[:2]<='26':
+        if 0<int(s[:2])<=26:
             s2 = self.numDecodings(s[2:])
         return s1+s2
+# if __name__ == "__main__":
+    # print(Solution().numDecodings('12120'))
