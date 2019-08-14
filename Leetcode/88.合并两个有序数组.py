885c1727fcbbf8db6e785cdea464fca0d1e2f965
hcxgit <812591452@qq.com>
2019-08-14 15:25:29 +0800

    merge

---
+++ Leetcode/88.合并两个有序数组.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=88 lang=python3
+#
+# [88] 合并两个有序数组
+#
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        两个指针，从后往前扫描
+        """
+        m,n,mer = m-1,n-1,len(nums1)-1
+        while m >= 0 and n >= 0:
+            if nums2[n]>=nums1[m]:
+                nums1[mer] = nums2[n]
+                n -= 1
+            else:
+                nums1[mer] = nums1[m]
+                m -= 1
+            mer -= 1
+        while n >= 0:
+            nums1[mer] = nums2[n]
+            n -= 1
+            mer -= 1
+        return nums1
