6458e4d904eef9e0709979cb1cefd46364ac6e3c
hcxgit <812591452@qq.com>
2019-08-19 14:45:10 +0800

    默认

--- beike.py
+++ beike.py
@@ -1,62 +1,50 @@
-# coding=utf-8
-import sys
+# # coding=utf-8
+# import sys
 #
+# N = int(sys.stdin.readline().strip())
 # alist = list(map(int, sys.stdin.readline().strip().split()))
-# if max(alist)==1:
-#     frist,second = 0,0
-#     for i in range(len(alist)):
-#         if alist[i] == 1:
-#             frist = i
-#         if alist[i] == 0:
-#             second = i
-#     print(frist, second)
-# else:
-#     index = alist.index(max(alist))+1
-#     for i in range(len(alist)):
-#         if alist[i] == index+1:
-#             frist = index*10+i
-#     if frist//10==2:
-#         res = []
-#         for i in range(len(alist)):
-#             if i ==0 and alist[0] != 2:
-#                 res.append(i)
-#             elif i ==1 and alist[1] != 13:
-#                 res.append(i)
-#             elif i == 2 and alist[2] != 10:
-#                 res.append(i)
-#             elif i not in {0,1,2} and alist[i] != 3:
-#                 res.append(i)
-#     print(frist,res[0]*10+res[1])
-#     print(frist,res[1]*10+res[0])
 #
+# if N == 1:
+#     print(alist[0],1)
+# else:
+#     dp = [i for i in alist]
+#     flag = [1 if i!=0 else 0 for i in alist ]
+#
+#     for i in range(2, N):
+#         if dp[i]==0:
+#             continue
+#         maxx = i-2
+#         for j in range(i-2,-1,-1):
+#             if dp[j]>dp[maxx]:
+#                 maxx = j
+#         dp[i] += dp[maxx]
+#         flag[i] += flag[maxx]
+#
+#     max_index = [i for i in range(len(dp)) if dp[i] == max(dp)]
+#     print(max(dp),min([flag[i] for i in max_index]))
 
+import sys
 
-flag = True
-while True:
-    res = []
-    s = sys.stdin.readline().strip()
-    s = s.split('"')
-    def cou(s):
-        n = 0
-        for i in s:
-            if i ==',':
-                n +=1
-        return n
-    for i in s:
-        c = cou(i)
-        if c == 1:
-            res.append(i+'"')
-        elif c>1:
-            i = i.split(',')
-            for j in range(len(i)):
-                if i[j]!='':
-                    res.append(i[j])
-                elif i[j]=='' and j!=len(i)-1:
-                    res.append('--')
-    if len(res) != 0:
-        print(len(res))
-        for i in res:
-            print(i)
-    else:
-        print(-1)
-        flag = False
+[N,T,M] = list(map(int,sys.stdin.readline().strip().split()))
+alist = list(map(int, sys.stdin.readline().strip().split()))
+def iskill(alist, T, M, X):
+    for i in alist:
+        t = i//X
+        if i%X ==1:
+            T -= (t+1)
+            M -= t
+        elif i%X ==0:
+            T -= t
+            M -= t
+        else:
+            T -= (t+1)
+            M -= (t+1)
+        if T < 0:
+            return False
+    return True
+res = -1
+for i in range(1,8):
+    if iskill(alist,T,M,i):
+        res = i
+        break
+print(res)
