71ff42eedc5153ce3402461483d590789fdcc1a7
hcxgit <812591452@qq.com>
2021-10-22 06:22:35 +0800

    添加java版二分查找、树的前中后序和层次遍历

---
+++ sort/src/BinarySearch.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+/**
+ * @author 三笠阿克曼
+ * @date 2021/10/21
+ * 二分查找
+ */
+public class BinarySearch {
+
+
+    //递归
+    public static int binarySearch(int[] arrays, int target, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+
+        int mid = (left + right) / 2;
+        if (arrays[mid] == target) {
+            return mid;
+        } else if (arrays[mid] > target) {
+            return binarySearch(arrays, target, left, mid - 1);
+        } else {
+            return binarySearch(arrays, target, mid + 1, right);
+        }
+    }
+
+    //非递归
+    public static int binarySearch(int[] arrays, int target) {
+        int left = 0;
+        int right = arrays.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arrays[mid] == target) {
+                return mid;
+            } else if (arrays[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] list = {1, 4, 2, 1, 3, 3, 9};
+        QuickSort.quickSort(list, 0, list.length - 1);
+        System.out.println(Arrays.toString(list));
+        int res = binarySearch(list, 4, 0, list.length - 1);
+        int res2 = binarySearch(list, 4);
+        System.out.println(res);
+        System.out.println(res2);
+    }
+}
---
+++ sort/src/TreeTraveral.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author 三笠阿克曼
+ * @date 2021/10/22
+ */
+public class TreeTraveral {
+
+    //先序遍历：递归
+    public static void preOrder(TreeNode root, ArrayList<Integer> list) {
+        if (root == null) {
+            return;
+        }
+
+        list.add(root.getdata());
+        if (root.getLeft() != null) {
+            preOrder(root.getLeft(), list);
+        }
+        if (root.getRight() != null) {
+            preOrder(root.getRight(), list);
+        }
+    }
+
+    //中序遍历：递归
+    public static void midOrder(TreeNode root, ArrayList<Integer> list) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.getLeft() != null) {
+            midOrder(root.getLeft(), list);
+        }
+        list.add(root.getdata());
+        if (root.getRight() != null) {
+            midOrder(root.getRight(), list);
+        }
+    }
+
+
+    //后序遍历：递归
+    public static void postOrder(TreeNode root, ArrayList<Integer> list) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.getLeft() != null) {
+            postOrder(root.getLeft(), list);
+        }
+        if (root.getRight() != null) {
+            postOrder(root.getRight(), list);
+        }
+        list.add(root.getdata());
+    }
+
+    public static void levelOrder(TreeNode root, ArrayList<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            res.add(node.getdata());
+            if (node.getLeft() != null) {
+                queue.offer(node.getLeft());
+            }
+            if (node.getRight() != null) {
+                queue.offer(node.getRight());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(node4, node5, 2);
+        TreeNode node1 = new TreeNode(node2, node3, 1);
+
+        ArrayList list = new ArrayList<Integer>(5);
+//        preOrder(node1, list);
+//        midOrder(node1, list);
+//        postOrder(node1, list);
+        levelOrder(node1, list);
+        System.out.println(list);
+    }
+}
