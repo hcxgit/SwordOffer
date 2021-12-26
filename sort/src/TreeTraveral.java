4462a0c6ef6705457f19fa12c27ef6926c18f957
hcxgit <812591452@qq.com>
2021-12-27 01:27:28 +0800

    添加leetcode:128

--- sort/src/TreeTraveral.java
+++ sort/src/TreeTraveral.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 /**
  * @author 三笠阿克曼
@@ -23,6 +24,26 @@ public class TreeTraveral {
         }
     }
 
+    //先序遍历：非递归   1、访问根节点 2、右子树压栈，左子树压栈 3、出栈
+    public static void preOrder2(TreeNode root, ArrayList<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        TreeNode temp = null;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            temp = stack.pop();
+            list.add(temp.getdata());
+            if (temp.getRight() != null) {
+                stack.push(temp.getRight());
+            }
+            if (temp.getLeft() != null) {
+                stack.push(temp.getLeft());
+            }
+        }
+    }
+
     //中序遍历：递归
     public static void midOrder(TreeNode root, ArrayList<Integer> list) {
         if (root == null) {
@@ -35,6 +56,28 @@ public class TreeTraveral {
         list.add(root.getdata());
         if (root.getRight() != null) {
             midOrder(root.getRight(), list);
+        }
+    }
+
+    //中序遍历：非递归   1、一直找左子树压栈，找不到后出栈，访问值，右子树压栈，继续循环
+    public static void midOrder2(TreeNode root, ArrayList<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        TreeNode temp = null;
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+
+            while (root != null) {
+                stack.push(root);
+                root = root.getLeft();
+            }
+
+            if (!stack.isEmpty()) {
+                temp = stack.pop();
+                list.add(temp.getdata());
+                root = temp.getRight();
+            }
         }
     }
 
@@ -82,9 +125,11 @@ public class TreeTraveral {
 
         ArrayList list = new ArrayList<Integer>(5);
 //        preOrder(node1, list);
+//        preOrder2(node1, list);
 //        midOrder(node1, list);
+        midOrder2(node1, list);
 //        postOrder(node1, list);
-        levelOrder(node1, list);
+//        levelOrder(node1, list);
         System.out.println(list);
     }
 }
---
+++ sort/src/leetcode/longestConsecutive.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author 三笠阿克曼
+ * @date 2021/12/27
+ * Leetcode: 128  最长连续序列
+ * 问题：给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度
+ * 方案：哈希表
+ */
+public class longestConsecutive {
+
+    public static int solution(int[] nums) {
+
+        Set<Integer> num_set = new HashSet<>();
+        int longestConsecutive = 0;
+        for (int num : nums) {
+            num_set.add(num);
+        }
+
+        //遍历查找以当前数为起点的连续序列的长度（如果有更小的，则表示该数不是起点，跳过该值）
+        for (int num : num_set) {
+            //找连续序列的最小值，还有更小的则跳过
+            if (!num_set.contains(num - 1)) {
+                int currentnum = 1;
+                //查找比该值大的x+1,x+2.....最长序列
+                while (num_set.contains(num + 1)) {
+                    currentnum++;
+                    num++;
+                }
+
+                longestConsecutive = Math.max(currentnum, longestConsecutive);
+            }
+        }
+        return longestConsecutive;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        int[] nums2 = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+        System.out.println(longestConsecutive.solution(nums2));
+    }
+}
