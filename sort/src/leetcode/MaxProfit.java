252e0724dc6269cad34260774ccad3f8557f7061
hcxgit <812591452@qq.com>
2022-07-22 16:58:13 +0800

    优化 leetcode 121：买卖股票的最佳时机

--- sort/src/leetcode/MaxProfit.java
+++ sort/src/leetcode/MaxProfit.java
@@ -5,24 +5,39 @@ package leetcode;
  * @date 2022/3/21
  *  Leetcode 121:买股票的最佳时机1（只能买卖一次）
  *
- *  动态规划：
- *    f(i+1) 表示第i+1天买入时的最大利润,
- *    f(i+1)和f(i)的差距就是那两天价格的差距，所以，f(i) = max(0,nums[i+1]-nums[i]+f(i+1))   最小为0
- *   由于只用到后一天的利润，可以优化只用一个变量表示。
+ *  1、标准dp
+ *      dp[i] 表示第i天卖出的最大利润
+ *      第【i天】和【i-1天】的【股票的价格差】其实就是【利润差】
+ *      dp[i] = max(0,dp[i-1]+nums[i]-nums[i-1])
+ *
+ *  2、优化版dp
+ *     只用到前一天的利润，优化为一个变量dp0表示。
  *
  */
 public class MaxProfit {
+
+    //1、标准dp
     public int maxProfit(int[] prices) {
+        int length = prices.length,res = 0;
+        int [] dp = new int[length];
+        dp[0] = 0;
 
-        //后一天买入的最大利润
-        int nextMaxProfit = 0;
-        int res = 0;
+        for(int i=1; i<length; i++){
+            dp[i] = Math.max(0,dp[i-1]+prices[i]-prices[i-1]);
+            res = Math.max(dp[i],res);
+        }
+        return res;
+    }
 
-        for(int i=prices.length-2; i>=0; i--){
-            //后一天和当天价格的差值  就是利润的差值，利润最小为0
-            int gap = prices[i+1]-prices[i];
-            nextMaxProfit = Math.max(0,nextMaxProfit + gap);
-            res = Math.max(nextMaxProfit,res);
+    //2、优化dp
+    public int maxProfit2(int[] prices) {
+
+        int length = prices.length,res = 0;
+        int dp0 = 0;
+
+        for(int i=1; i<length; i++){
+            dp0 = Math.max(0,dp0 + prices[i]-prices[i-1]);
+            res = Math.max(dp0, res);
         }
         return res;
     }
