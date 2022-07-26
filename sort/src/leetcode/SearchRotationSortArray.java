a794da81523924210b3e893b5f7de6dd08ea4b75
hcxgit <812591452@qq.com>
2022-07-26 16:29:21 +0800

    Leetcode 33：搜索旋转排序数组

---
+++ sort/src/leetcode/SearchRotationSortArray.java
@@ -0,0 +1,55 @@
+package leetcode;
+
+/**
+ * @author 三笠阿克曼
+ * @date 2022/7/26
+ *
+ *
+ * Leetcode 33：搜索旋转排序数组
+ *
+ *  1、二分查找
+ *
+ *      二分后，【一半有序】，【一半部分有序】。
+ *
+ *      注意：边界值
+ */
+public class SearchRotationSortArray {
+    public int search(int[] nums, int target) {
+
+        int len = nums.length;
+
+        if(len == 0){
+            return -1;
+        }
+        if(len==1 && target == nums[0]){
+            return target == nums[0]? 0:-1;
+        }
+
+        int left = 0, right = len-1, mid;
+        while (left <= right){
+            mid = (left + right)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(nums[mid] < target){
+                // mid在右递增数组 && target > right, 左边找
+                if(nums[mid] < nums[left] && target > nums[right]){
+                    right = mid - 1;
+                }else{
+                    // 其余右边找
+                    left = mid + 1;
+                }
+            }else{
+                // mid在左递增数组 && target < left, 右边找
+                if(nums[mid] >= nums[left] && target < nums[left]){
+                    // 右边找
+                    left = mid + 1;
+                }else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
