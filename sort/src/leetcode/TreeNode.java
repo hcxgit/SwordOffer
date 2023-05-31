c61a4968eca1f7c3b564f80dc1d7d73dab659aea
hcxgit <812591452@qq.com>
2023-05-31 18:49:36 +0800

    add TreeNode

---
+++ sort/src/leetcode/TreeNode.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * @author 三笠阿克曼
+ * @date 2023/5/31
+ * @description
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
