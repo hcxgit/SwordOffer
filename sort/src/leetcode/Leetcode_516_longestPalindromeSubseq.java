a79213f258c8c0ea48ce0b6e3a484e0c98dab30f
hcxgit <812591452@qq.com>
2023-06-06 16:54:02 +0800

    add Leetcode 516 : 最长回文子序列

---
+++ sort/src/leetcode/Leetcode_516_longestPalindromeSubseq.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+/**
+ * @author 三笠阿克曼
+ * @date 2023/6/6
+ * @description Leetcode 516 : 最长回文子序列
+ */
+public class Leetcode_516_longestPalindromeSubseq {
+    public int longestPalindromeSubseq(String s) {
+        /**
+         dp[i][j]: [i,j] 范围内的最长的回文子序列长度
+
+         如果 s[i] == s[j]:
+         dp[i][j] = dp[i+1][j-1] +2
+         如果 s[i] != s[j]:
+         dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+         */
+
+        int len = s.length();
+        int[][] dp = new int[len][len];
+        dp[len-1][len-1] = 1;
+        for(int i=len-2;i>=0;i--){
+            for(int j=i+1;j<len;j++){
+                // 初始化 dp[i][i] = 1
+                dp[i][i] = 1;
+                if(s.charAt(i) == s.charAt(j)){
+                    dp[i][j] = dp[i+1][j-1] +2;
+                }else{
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][len-1];
+    }
+}
