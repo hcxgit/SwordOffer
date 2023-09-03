425c9ac5898e243bbe1d75751d2dbc4b929639f3
hcxgit <812591452@qq.com>
2023-09-03 16:27:18 +0800

    update Leetcode 121:买股票的最佳时机1（只能买卖一次）

--- sort/src/leetcode/Leetcode_121_maxProfit.java
+++ sort/src/leetcode/Leetcode_121_maxProfit.java
@@ -5,19 +5,24 @@ package leetcode;
  * @date 2022/3/21
  *  Leetcode 121:买股票的最佳时机1（只能买卖一次）
  *
- *  1、标准dp
- *      dp[i] 表示第i天卖出的最大利润
- *      第【i天】和【i-1天】的【股票的价格差】其实就是【利润差】
- *      dp[i] = max(0,dp[i-1]+nums[i]-nums[i-1])
+ *      1、标准dp
+ *          dp[i] 表示第i天卖出的最大利润
+ *          第【i天】和【i-1天】的【股票的价格差】其实就是【利润差】
+ *          dp[i] = max(0,dp[i-1]+nums[i]-nums[i-1])
  *
- *  2、优化版dp
- *     只用到前一天的利润，优化为一个变量dp0表示。
+ *      2、优化版dp
+ *          只用到前一天的利润，优化为一个变量dp0表示。
  *
+ *
+ *      3、一次遍历
+ *          - 第i天卖出能获得的最大利润：当第(0,i-1)天的【最低点买入】。
+ *          - 所以，遍历时，记录preMin值，则能知道【第i天卖出的最大利润】
+ *          - 同时更新 res = max(res,prices[i] - preMin), preMin = min(preMin,nums[i])
  */
 public class Leetcode_121_maxProfit {
 
     //1、标准dp
-    public int maxProfit(int[] prices) {
+    public int maxProfit_1(int[] prices) {
         int length = prices.length,res = 0;
         int [] dp = new int[length];
         dp[0] = 0;
@@ -30,7 +35,7 @@ public class Leetcode_121_maxProfit {
     }
 
     //2、优化dp
-    public int maxProfit2(int[] prices) {
+    public int maxProfit_2(int[] prices) {
 
         int length = prices.length,res = 0;
         int dp0 = 0;
@@ -39,6 +44,20 @@ public class Leetcode_121_maxProfit {
             dp0 = Math.max(0,dp0 + prices[i]-prices[i-1]);
             res = Math.max(dp0, res);
         }
+        return res;
+    }
+
+    /**
+     3、一次遍历
+     */
+    public int maxProfit(int[] prices) {
+        int preMin = prices[0];
+        int res = 0;
+        for(int i=1; i<prices.length; i++){
+            res = Math.max(res,prices[i] - preMin);
+            preMin = Math.min(preMin,prices[i]);
+        }
+
         return res;
     }
 }
